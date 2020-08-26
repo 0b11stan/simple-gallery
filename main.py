2c04b9320110b3db7bbb6301854341c3ad76d8ff
TristanPinaudeau <tristan.pinaudeau@gmail.com>
2020-08-27 00:57:57 +0200

    fix: refactor routes

--- main.py
+++ main.py
@@ -15,24 +15,24 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def gallery():
-    if request.method == 'GET':
-        images = [splitext(f) for f in listdir(IMAGES_PATH) if isfile(
-            join(IMAGES_PATH, f)
-        )]
-        print(images)
-        return render_template('gallery.html.j2',
-                               images=images)
-    elif request.method == 'POST':
-        if 'file' not in request.files:
-            #flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            #flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('gallery'))
+    images = [splitext(f) for f in listdir(IMAGES_PATH) if isfile(
+        join(IMAGES_PATH, f)
+    )]
+    return render_template('gallery.html.j2', images=images)
+
+
+@app.route('/pic', methods=['POST'])
+def pic():
+    if 'file' not in request.files:
+        #flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        #flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('gallery'))
